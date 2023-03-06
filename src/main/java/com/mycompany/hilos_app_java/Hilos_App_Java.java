9bf6c9980310f536bb9c27d2bbbecd9a9b34c796
Dann-13 <dannyelgomezz.13@gmail.com>
2023-03-06 15:29:21 -0500

    Subiendo Proyecto

---
+++ src/main/java/com/mycompany/hilos_app_java/Hilos_App_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hilos_app_java;
+
+/**
+ *
+ * @author danndev
+ */
+public class Hilos_App_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
